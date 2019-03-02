d42cb4a5bb22c6f1da4366d13244c28fb1ec260b
marcioquimbundo <marcioquimbundo@hotmail.com>
2019-03-02 12:10:11 +0100

    homePage created and finished

--- lib/src/widgets/home_page.dart
+++ lib/src/widgets/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_infinit_list_bloc/src/blocs/bloc.dart';
 
 class HomePage extends StatefulWidget {
   final Widget child;
@@ -9,10 +13,56 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final _scrollController = ScrollController();
+  final PostBloc _postBloc = PostBloc(httpClient: http.Client());
+  final _scrollThreshold = 200.0;
+
+  _HomePageState() {
+    _scrollController.addListener(_onScroll);
+    _postBloc.dispatch(Fetch());
+  }
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: widget.child,
+    return BlocBuilder(
+      bloc: _postBloc,
+      builder: (BuildContext context, PostState state) {
+        if (state is PostUninitialized) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        if (state is PostError) {
+          return Center(
+            child: Text('failed to fetch posts'),
+          );
+        }
+        if (state is PostLoaded) {
+          if (state.posts.isEmpty) {
+            return Center(
+              child: Text('No Posts'),
+            );
+          }
+          return ListView.builder(
+            itemBuilder: (BuildContext context, int index){
+              return null;/*index >= state.posts.length
+                ? BottomLoader()
+                : PostWidget(post: state.posts[index]);*/
+            },
+            itemCount: state.hasReachedMax
+              ? state.posts.length
+              : state.posts.length + 1,
+            controller: _scrollController,
+          );
+        }
+      },
     );
+  }
+
+  void _onScroll() {
+    final maxScroll = _scrollController.position.maxScrollExtent;
+    final currentScroll =_scrollController.position.pixels;
+    if (maxScroll - currentScroll <=_scrollThreshold) {
+      _postBloc.dispatch(Fetch());
+    }
   }
 }
